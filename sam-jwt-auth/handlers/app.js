738366e46acfa79b8709589bfecbed357d246209
Aadityakiran S <aadityakirans@gmail.com>
2023-04-18 11:33:51 +0530

    Fixed some issues with MongoDB connection

--- sam-jwt-auth/handlers/app.js
+++ sam-jwt-auth/handlers/app.js
@@ -4,16 +4,16 @@ require('dotenv').config();
 
 exports.connectToMongoDB = async (event, context) => {
     response = {
-        'statusCode': 200,
-        'body': JSON.stringify({
-            message: 'Successfully connected to DB',
-        })
-    }
+        statusCode: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: `Nothing at the moment`
+    };
     try {
         await connectDB(process.env.MONGO_URI);
+        response.body = `Successfully connected to MongoDB`;
     } catch (err) {
         console.log(err);
-        return err;
+        response.body = JSON.stringify(err);
     }
 
     return response
